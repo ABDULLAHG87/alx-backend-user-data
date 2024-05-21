688217e3be4377e25a71f326ff5e7cbb1eef77ac
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-21 22:25:23 +0000

    BUGFIX: fix error in the index file

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,11 +5,17 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/unauthorized', method=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unathorized() -> str:
     """methods for unauthorized"""
     abort(401, description="Unauthorized")
-    
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+@def forbidden() -> str:
+    """method for forbidden"""
+    abort(403, description='Forbidden')
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
