12a35ec16ea3711f29dd90a173f5357d3ae6a3ad
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-23 21:36:05 +0000

    FEAT: add all files from project 0x01-Basic_authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Module for Authentication in Flask
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Defining the Auth Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method for require authentication"""
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == '*':
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Method for authorization_header"""
+        if request is None:
+            return None
+
+        header = request.headers.get('Authorization')
+
+        if header is None:
+            return None
+
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Method for current user"""
+        return None
