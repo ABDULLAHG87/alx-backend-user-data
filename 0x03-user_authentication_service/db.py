0c52a2b4240e5875630d73322adb8d145d2b55fc
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-29 07:21:13 +0000

    BUGFIX: fix errors within the db.py code

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -31,7 +31,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Method for adding user"""
-        new_user = User(email=emial, hashed_password=hashed_password)
+        new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
