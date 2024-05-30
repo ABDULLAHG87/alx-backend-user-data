40dae51c4f0d15813f2424dd0256bf04c32dcdc6
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-30 07:32:37 +0000

    FEAT: add feature for Basic Flask task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Flask application Program"""
+
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+AUTH = Auth()
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index() -> str:
+    """Method for the index route"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """Method for users routing"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@pp.route('/sessions', methods=['POST'])
+def login() -> str:
+    """Login route definition"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        response = jsonify({'email': email, 'message': 'logged in'})
+        response.set_cookie('session_id', session_id)
+
+    return response
+
+
+if __name__ = '__main__':
+    app.run(host='0.0.0.0', port='5000')
