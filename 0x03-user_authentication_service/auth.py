ced26a9dcc2f0d8a0eec4ed5b830aea59f8f0a43
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-30 06:50:43 +0000

    FEAT: fix error related to pycodestyle with auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,7 +37,6 @@ class Auth:
         else:
             raise ValueError('User {} already exists'.format(email))
 
-
     def valid_login(self, email: str, password: str) -> bool:
         """Method for valid login"""
         try:
@@ -45,7 +44,6 @@ class Auth:
         except NoResultFound:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
-
 
     def create_session(self, email: str) -> str:
         """Creating Session for email"""
@@ -80,7 +78,7 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """"Method for getting password reset"""
-        
+
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
