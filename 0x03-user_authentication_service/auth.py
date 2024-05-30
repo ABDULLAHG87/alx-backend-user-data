88b0fa3f85ecc5d762cc683a089812f60490fee0
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-30 06:44:16 +0000

    FEAT: add the register user features to the auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,8 +33,19 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError('User {} already exists'.format(email))
+
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Method for valid login"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
 
     def create_session(self, email: str) -> str:
         """Creating Session for email"""
@@ -45,3 +56,46 @@ class Auth:
         else:
             user.session_id = _generate_uuid()
             return user.session_id
+
+    def get_user_from_session_id(self, user_id: str) -> User:
+        """Method for get user from session id"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
+
+    def destroy_session(self, user_id: str) -> None:
+        """Method for destroying session"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """"Method for getting password reset"""
+        
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Method for updating password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
